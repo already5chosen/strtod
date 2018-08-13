dbda0aa90ec131b4bcf0d3de52ec0c803cd5870a
Michael S <already5chosen@yahoo.com>
2018-08-14 02:04:06 +0300

    n2_small_strtod - more improvements
    Get rid off comparison with non-constant maxVal.
    Define maxVal constant to have 32 LS bits == 0, to simplify code,
    generated for comparison.
    Restructure code to remove remaining gotos.
    
    The size is back to where it was before previous round of changes and even
    a little lower, but the code is now easier to follow.
    Easier, not easy.

--- n2_small_strtod.c
+++ n2_small_strtod.c
@@ -77,11 +77,10 @@ small_strtod(const char* str, char** endptr)
 {
   const char* p = skipWhiteSpaces(str);
 
-  uint64_t uret;
   const char* endptrval = 0;
   uint64_t  rdVal = 0;
   ptrdiff_t rdExp = 0, exp;
-  uint64_t  maxVal = (UINT64_MAX-9)/10;
+  const uint64_t  maxVal = (((UINT64_MAX-9)/10)>>32)<<32;
   uint64_t mant;
   int lsbits = 0;
   int sticky;
@@ -102,7 +101,7 @@ small_strtod(const char* str, char** endptr)
           break; // non-digit
         endptrval = p;
         rdExp += parseState;
-        if (rdVal <= maxVal) {
+        if (rdVal < maxVal) {
           rdVal =
             rdVal*10+dig;
           --rdExp;
@@ -114,6 +113,10 @@ small_strtod(const char* str, char** endptr)
       // non-digit
       if (parseState == PARSE_EXP)
         goto end_of_parser;
+
+      neg     = nege;
+      exp     = rdExp;
+      sticky  = (lsbits != 0);
 
       if (parseState == PARSE_INT) {
         if (c == '.') {
@@ -133,118 +136,114 @@ small_strtod(const char* str, char** endptr)
       }
 
       // conversion succeed
-      neg     = nege;
-      exp     = rdExp;
       mant    = rdVal;
-      sticky  = (lsbits != 0);
       rdVal   = 0;
       if (c == 'e' || c == 'E') {
         // possibly, exponent present
-        maxVal = (uint64_t)1 << 32;
         break;
       }
 
       goto end_of_parser;
     }
   }
-  end_of_parser:;
+  end_of_parser:
   if (endptr)
     *endptr = (char*)endptrval;
 
-  uret = 0;
+  uint64_t uret = (uint64_t)neg << 63; // set sign bit
   if (mant != 0) {
-    uint32_t w2 = (uint32_t)(mant >> 32);
-    uint32_t w1 = (uint32_t)mant;
-    uint32_t w0 = 0;
-    int bine = 63;
-    if (w2 == 0) {
-      w2   = w1;
-      w1   = 0;
-      bine = 63 - 32;
-    }
-
     const int MAX_EXP_MAGNITUDE = 345;
+    if (rdVal > UINT32_MAX)
+      rdVal = UINT32_MAX;
     int64_t exp64 = nege==0 ? rdVal : -rdVal;
     exp64 += exp;
     unsigned sexp = exp64 < 0;
     if (sexp)
       exp64 = -exp64;
+
     if (exp64 >= MAX_EXP_MAGNITUDE) {
+      // exponent out of range
       if (sexp == 0)
-        uret = (uint64_t)2047 << 52; // Inf
-      goto done;
+        uret |= (uint64_t)2047 << 52; // Inf
+    } else {
+      unsigned mexp = exp64;
+
+      uint32_t w2 = (uint32_t)(mant >> 32);
+      uint32_t w1 = (uint32_t)mant;
+      uint32_t w0 = 0;
+      int bine = 63;
+      if (w2 == 0) {
+        w2   = w1;
+        w1   = 0;
+        bine = 63 - 32;
+      }
+      for (;;) {
+        // normalize mantissa
+        bine -= normailize96(&w2, &w1, &w0);
+
+        if (mexp == 0)
+          break;
+
+        // scale w2:w1:w0 * 2**bine by 10**exp
+        typedef struct {
+          uint32_t MULx_H;
+          uint32_t MULx_M;
+          uint8_t  MULx_L;
+          uint8_t  decExp;
+          int16_t  binExp;
+        } dec_scale_tab_entry_t;
+        static const dec_scale_tab_entry_t DecScaleTab[2][3] = {
+          { // scale up
+            { 0xFC6F7C40, 0x45812296, 0x4D, 31, 103,}, // (10**31/2**103)*2**72
+            { 0xFA000000, 0x00000000, 0x00,  3,  10,}, // (10**3 /2**10 )*2**72
+            { 0xA0000000, 0x00000000, 0x00,  1,   4,}, // (10**0 /2**4  )*2**72
+          },
+          { // scale down
+            { 0xE45C10C4, 0x2A2B3B05, 0x8D, 44,-146,}, // (2**146/10**44)*2**72
+            { 0xD6BF94D5, 0xE57A42BC, 0x3D,  7, -23,}, // (2**23 /10**7 )*2**72
+            { 0xCCCCCCCC, 0xCCCCCCCC, 0xCD,  1,  -3,}, // (2**3  /10**1 )*2**72
+          },
+        };
+        const dec_scale_tab_entry_t* pTab = &DecScaleTab[sexp][0];
+        unsigned decExp;
+        while ((decExp=pTab->decExp) > mexp)
+          ++pTab;
+
+        const uint32_t MULx_L = pTab->MULx_L;
+        const uint32_t MULx_M = pTab->MULx_M;
+        const uint32_t MULx_H = pTab->MULx_H;
+        const int      binExp = pTab->binExp;
+        do {
+          // multiply by 10**N, where N= (-44, -7, -1, 1, 3, 31)
+          // w2*mh
+          //    w2*mm
+          //       w2*ml
+          //    w1*mh
+          //       w1*mm
+          //       w0*mh
+          uint64_t w2w1 =
+                ( (uint64_t)w2 * MULx_H)
+              + (((uint64_t)w2 * MULx_M) >> 32)
+              + (((uint64_t)w1 * MULx_H) >> 32);
+          w0 =  (w0 >> 18) * (MULx_H >> 17)
+              +((w1        *  MULx_H) >> 3)
+              +((w2        *  MULx_M) >> 3)
+              + (w1 >> 18) * (MULx_M >> 17)
+              + (w2 >> 11) *  MULx_L;
+          w2w1 += (w0 >> 29);
+          w0  = w0 << 3;
+          w1  = (uint32_t)w2w1;
+          w2  = (uint32_t)(w2w1 >> 32);
+          bine += binExp;
+          mexp -= decExp;
+        } while (mexp >= decExp);
+      }
+      // printf("%08x:%08x:%08x\n", w2, w1, w0);
+      w1 |= ((w0>>22) != 0); // approximately 9-10 MS bits of w0 are good. The rest is garbage
+      w1 |= sticky;
+      uret |= to_double(((uint64_t)w2 << 32) | w1, bine);
     }
-    unsigned mexp = exp64;
-
-    for (;;) {
-      // normalize mantissa
-      bine -= normailize96(&w2, &w1, &w0);
-
-      if (mexp == 0)
-        break;
-
-      // scale w2:w1:w0 * 2**bine by 10**exp
-      typedef struct {
-        uint32_t MULx_H;
-        uint32_t MULx_M;
-        uint8_t  MULx_L;
-        uint8_t  decExp;
-        int16_t  binExp;
-      } dec_scale_tab_entry_t;
-      static const dec_scale_tab_entry_t DecScaleTab[2][3] = {
-        { // scale up
-          { 0xFC6F7C40, 0x45812296, 0x4D, 31, 103,}, // (10**31/2**103)*2**72
-          { 0xFA000000, 0x00000000, 0x00,  3,  10,}, // (10**3 /2**10 )*2**72
-          { 0xA0000000, 0x00000000, 0x00,  1,   4,}, // (10**0 /2**4  )*2**72
-        },
-        { // scale down
-          { 0xE45C10C4, 0x2A2B3B05, 0x8D, 44,-146,}, // (2**146/10**44)*2**72
-          { 0xD6BF94D5, 0xE57A42BC, 0x3D,  7, -23,}, // (2**23 /10**7 )*2**72
-          { 0xCCCCCCCC, 0xCCCCCCCC, 0xCD,  1,  -3,}, // (2**3  /10**1 )*2**72
-        },
-      };
-      const dec_scale_tab_entry_t* pTab = &DecScaleTab[sexp][0];
-      unsigned decExp;
-      while ((decExp=pTab->decExp) > mexp)
-        ++pTab;
-
-      const uint32_t MULx_L = pTab->MULx_L;
-      const uint32_t MULx_M = pTab->MULx_M;
-      const uint32_t MULx_H = pTab->MULx_H;
-      const int      binExp = pTab->binExp;
-      do {
-        // multiply by 10**N, where N= (-44, -7, -1, 1, 3, 31)
-        // w2*mh
-        //    w2*mm
-        //       w2*ml
-        //    w1*mh
-        //       w1*mm
-        //       w0*mh
-        uint64_t w2w1 =
-              ( (uint64_t)w2 * MULx_H)
-            + (((uint64_t)w2 * MULx_M) >> 32)
-            + (((uint64_t)w1 * MULx_H) >> 32);
-        w0 =  (w0 >> 18) * (MULx_H >> 17)
-            +((w1        *  MULx_H) >> 3)
-            +((w2        *  MULx_M) >> 3)
-            + (w1 >> 18) * (MULx_M >> 17)
-            + (w2 >> 11) *  MULx_L;
-        w2w1 += (w0 >> 29);
-        w0  = w0 << 3;
-        w1  = (uint32_t)w2w1;
-        w2  = (uint32_t)(w2w1 >> 32);
-        bine += binExp;
-        mexp -= decExp;
-      } while (mexp >= decExp);
-    }
-    // printf("%08x:%08x:%08x\n", w2, w1, w0);
-    w1 |= ((w0>>22) != 0); // approximately 9-10 MS bits of w0 are good. The rest is garbage
-    w1 |= sticky;
-    uret = to_double(((uint64_t)w2 << 32) | w1, bine);
   }
-
-  done:
-  uret |= (uint64_t)neg << 63; // set sign bit
 
   double dret;
   memcpy(&dret, &uret, sizeof(dret));
