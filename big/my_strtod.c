88815a452e347aa4ea740c26af7f5deb47d215e2
Michael S <already5chosen@yahoo.com>
2021-03-29 20:34:27 +0300

    +big/my_strtod.c
    Code an easy part of strtod() - a parser and initial lower/upper estimates.
    For a hard part, i.e. a case when upper and lower estimate differ, so far I resort to clib implementation.

---
+++ big/my_strtod.c
@@ -0,0 +1,387 @@
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdlib.h>
+
+enum {
+  INPLEN_MAX = 100000, // maximal length of legal input string, not including leading whitespace characters and sign
+  PARSE_DIG  = 17,
+};
+
+static uint64_t quickCore(uint64_t mntL, uint64_t mntH, int decExp, bool* done);
+
+typedef struct {
+  uint64_t    mnt;
+  const char* nz0;    // first non-zero digit
+  const char* nzlast; // last non-zero digit
+  int         dotI;
+  int         eomI;   // end of part, accumulated within mnt
+  int         decExp;
+} parse_t;
+
+static double u2d(uint64_t x) {
+  double y;
+  memcpy(&y, &x, sizeof(y));
+  return y;
+}
+
+static const char* parseTail(parse_t* dst, const char* str, int i)
+{
+  if (dst->dotI < 0) // there was no dot
+    dst->dotI = i;   // implied dot after mantissa
+
+  const char* ret = &str[i];
+  switch (str[i]) {
+    case 'e':
+    case 'E':
+      break; // exponent
+
+    default:
+      return ret; // done
+  }
+
+  // exponent
+  ++i;
+  // process sign
+  char neg = str[i];
+  switch (neg) {
+    case '+':
+    case '-':
+      ++i;
+      break;
+    default:
+      break;
+  }
+
+  if ((unsigned)(str[i]-'0') > 9)
+    return ret; // no exponent, done
+
+  // look for the first non-zero digit
+  while (str[i] == '0') {
+    if (i == INPLEN_MAX)
+      return NULL; // input too long
+    ++i;
+  }
+
+  // accumulate decExp
+  int decExp = 0;
+  do {
+    unsigned char dig = *(unsigned char*)&str[i] - '0';
+    if (dig > 9)
+      break;
+    decExp = decExp * 10 + dig;
+    ++i;
+  } while (decExp < INPLEN_MAX*2);
+  if (neg=='-')
+    decExp = -decExp;
+  dst->decExp = decExp;
+
+  // look for the end of exponent
+  for (; i < INPLEN_MAX; ++i) {
+    if (*(unsigned char*)&str[i] - '0' > 9)
+      return &str[i]; // end found
+  }
+
+  return NULL; // input too long
+}
+
+static const char* parse(parse_t* dst, const char* str)
+{
+  int  i  = 0;
+  char c0 = str[0];
+  char dotC = '.';
+  dst->dotI = -1; // no dot
+  if ((unsigned)(c0-'0') > 9) {
+    if (c0 != dotC)
+      return NULL; // illegal input
+    dst->dotI = 1; // record the next index, when dot found before the end of mnt
+    dotC = '0';
+    if ((unsigned)(str[1]-'0') > 9)
+      return NULL; // illegal input
+    i = 1;
+  }
+  // input is legal unless too long
+
+  // look for the first non-zero digit
+  for (;;) {
+    while (str[i] == '0') {
+      if (i == INPLEN_MAX)
+        return NULL; // input too long
+      ++i;
+    }
+
+    c0 = str[i];
+    if ((unsigned)(c0-'0') <= 9)
+      break; // non-zero digit found
+
+    if (c0 != dotC)
+      return parseTail(dst, str, i);
+
+    // dot found
+    dst->dotI = i+1; // record the next index, when dot found before the end of mnt
+    dotC = '0';
+    ++i;
+    // continue
+  }
+
+  // accumulate mantissa
+  dst->nz0 = &str[i];
+  uint64_t mnt = 0;
+  for (int k = 0; k < PARSE_DIG;) {
+    unsigned char dig = *(unsigned char*)&str[i] - '0';
+    if (dig <= 9) {
+      mnt = mnt * 10 + dig;
+      ++k;
+    } else {
+      if (str[i] != dotC)
+        break;
+      // dot found
+      dst->dotI = i+1; // record the next index, when dot found before the end of mnt
+      dotC = '0';
+    }
+    ++i;
+  }
+  dst->mnt  = mnt;
+  dst->eomI = i;
+
+  // look for the end of mantissa
+  int nzlast_i = -1;
+  for (; i < INPLEN_MAX; ++i) {
+    c0 = str[i];
+    if (c0 >= '0' && c0 <= '9') {
+      if (c0 != '0')
+        nzlast_i = i;
+    } else {
+      if (c0 != dotC) {
+        // end of mantissa found
+        if (nzlast_i >= 0)
+          dst->nzlast = &str[nzlast_i];
+        return parseTail(dst, str, i);
+      }
+      // dot found
+      dst->dotI = i;
+      dotC = '0';
+    }
+  }
+
+  return NULL; // input too long
+}
+
+double my_strtod(const char* str, char** str_end)
+{
+  // discard leading whitespace characters
+  const char* pstr = str;
+  while (isspace(*pstr)) ++pstr;
+
+  // process sign
+  char neg = *pstr;
+  switch (neg) {
+    case '+':
+    case '-':
+      ++pstr;
+      break;
+    default:
+      break;
+  }
+
+  if (str_end)
+    *str_end = (char*)str;
+
+  parse_t prs = {0};
+  const char* endp= parse(&prs, pstr);
+  if (endp == 0)
+    return 0;
+
+  if (str_end)
+    *str_end = (char*)endp;
+
+  uint64_t signBit = (neg=='-') ? (uint64_t)1 << 63 : 0;
+  if (prs.mnt == 0)
+    return u2d(signBit);
+
+  int decExp = prs.decExp + prs.dotI - prs.eomI;
+
+  if (decExp < -324-PARSE_DIG)
+    return u2d(signBit);
+
+  const uint64_t uINF = (uint64_t)2047 << 52;
+  if (decExp > 308)
+    return u2d(uINF+signBit);
+
+  bool done;
+  uint64_t uRet = quickCore(prs.mnt, prs.mnt + (prs.nzlast != 0), decExp, &done);
+  if (done)
+    return u2d(uRet+signBit);
+
+  return strtod(str, str_end);
+}
+
+static uint64_t tab1[28] = { // 5**k
+  1ull,
+  5ull,
+  25ull,
+  125ull,
+  625ull,
+  3125ull,
+  15625ull,
+  78125ull,
+  390625ull,
+  1953125ull,
+  9765625ull,
+  48828125ull,
+  244140625ull,
+  1220703125ull,
+  6103515625ull,
+  30517578125ull,
+  152587890625ull,
+  762939453125ull,
+  3814697265625ull,
+  19073486328125ull,
+  95367431640625ull,
+  476837158203125ull,
+  2384185791015625ull,
+  11920928955078125ull,
+  59604644775390625ull,
+  298023223876953125ull,
+  1490116119384765625ull,
+  7450580596923828125ull,
+};
+
+static uint64_t tab28[25] = { // 10**((k-13)*28) * 2**ceil((13-k)*93.0139866568461+64)
+  0xe1afa13afbd14d6d, //  10**(-364) * 2**(64+1209)
+  0xe3e27a444d8d98b7, //  10**(-336) * 2**(64+1116)
+  0xe61acf033d1a45df, //  10**(-308) * 2**(64+1023)
+  0xe858ad248f5c22c9, //  10**(-280) * 2**(64+ 930)
+  0xea9c227723ee8bcb, //  10**(-252) * 2**(64+ 837)
+  0xece53cec4a314ebd, //  10**(-224) * 2**(64+ 744)
+  0xef340a98172aace4, //  10**(-196) * 2**(64+ 651)
+  0xf18899b1bc3f8ca1, //  10**(-168) * 2**(64+ 558)
+  0xf3e2f893dec3f126, //  10**(-140) * 2**(64+ 465)
+  0xf64335bcf065d37d, //  10**(-112) * 2**(64+ 372)
+  0xf8a95fcf88747d94, //  10**( -84) * 2**(64+ 279)
+  0xfb158592be068d2e, //  10**( -56) * 2**(64+ 186)
+  0xfd87b5f28300ca0d, //  10**( -28) * 2**(64+  93)
+  0x8000000000000000, //  10**(   0) * 2**(64-   1)
+  0x813f3978f8940984, //  10**(  28) * 2**(64-  94)
+  0x82818f1281ed449f, //  10**(  56) * 2**(64- 187)
+  0x83c7088e1aab65db, //  10**(  84) * 2**(64- 280)
+  0x850fadc09923329e, //  10**( 112) * 2**(64- 373)
+  0x865b86925b9bc5c2, //  10**( 140) * 2**(64- 466)
+  0x87aa9aff79042286, //  10**( 168) * 2**(64- 559)
+  0x88fcf317f22241e2, //  10**( 196) * 2**(64- 652)
+  0x8a5296ffe33cc92f, //  10**( 224) * 2**(64- 745)
+  0x8bab8eefb6409c1a, //  10**( 252) * 2**(64- 838)
+  0x8d07e33455637eb2, //  10**( 280) * 2**(64- 931)
+  0x8e679c2f5e44ff8f, //  10**( 308) * 2**(64-1024)
+};
+
+
+static uint64_t ldexp_u(uint64_t m2, int be)
+{
+  const uint64_t uINF = (uint64_t)2047 << 52;
+  be += 64+1023+63; // biased exponent
+  if (be > 1023*2)
+    return uINF;
+
+  uint64_t mnt = m2 >> 11;
+  int mnt_bits = 53;
+  if (be < 1) {
+    // subnormal
+    int rsh = 1-be;
+    mnt_bits -= rsh;
+    be = 0;
+    if (mnt_bits < 0)
+      return 0;
+    mnt >>= rsh;
+  }
+  uint64_t tail = m2 << mnt_bits;
+  tail |= (mnt & 1);           // break tie to even
+  mnt &= ((uint64_t)-1 >>12);  // mask out implied '1'
+  mnt |= (uint64_t)be << 52;   // + biased exponent
+  mnt += tail > ((uint64_t)1 << 63); // round to nearest
+  return mnt;
+}
+
+static uint64_t quickCore(uint64_t mntL, uint64_t mntU, int decExp, bool* done)
+{
+  int ie = decExp + 13*28;
+  int iH = ie / 28; // index in tab28
+  int iL = ie % 28; // index in tab1
+
+  // multiply mntL,mntH by 10**decExp
+  unsigned __int128 mxL = (unsigned __int128)mntL * tab1[iL];
+  unsigned __int128 mxU = (unsigned __int128)mntU * tab1[iL];
+  int beL = iL, beU = beL; // binary exponent
+  uint64_t m2L = (uint64_t)(mxL >> 64);
+  uint64_t m1L = (uint64_t)(mxL);
+  uint64_t m0L = 0;
+  uint64_t m2U = (uint64_t)(mxU >> 64);
+  uint64_t m1U = (uint64_t)(mxU);
+  uint64_t m0U = 0;
+  // up to this point multiplication is exact
+  if (iH != 13) {
+    // this multiplication is approximate, so we need different coefficients for upper and lower estimates
+    beL += (int)ceil((iH-13)*93.0139866568461);
+    beU = beL;
+    uint64_t x28 = tab28[iH];
+    unsigned __int128 mlL, mlU;
+    mlL = (unsigned __int128)m1L * x28;
+    mxL = (unsigned __int128)m2L * x28;
+    mxL += (uint64_t)(mlL >> 64);
+    m2L = (uint64_t)(mxL >> 64);
+    m1L = (uint64_t)(mxL);
+    m0L = (uint64_t)(mlL);
+
+    mlU = (unsigned __int128)m1U * (x28+1);
+    mxU = (unsigned __int128)m2U * (x28+1);
+    mxU += (uint64_t)(mlU >> 64);
+    m2U = (uint64_t)(mxU >> 64);
+    m1U = (uint64_t)(mxU);
+    m0U = (uint64_t)(mlU);
+
+    if (m2L == 0) {
+      beL -= 64;
+      m2L = m1L; m1L = m0L; m0L = 0;
+    }
+
+    if (m2U == 0) {
+      beU -= 64;
+      m2U = m1U; m1U = m0U; m0U = 0;
+    }
+  }
+
+  if (m2L == 0) {
+    beL -= 64;
+    m2L = m1L; m1L = 0;
+  }
+
+  if (m2U == 0) {
+    beU -= 64;
+    m2U = m1U; m1U = 0;
+  }
+
+  // normalize m2L:M1L
+  int lsh = __builtin_clzll(m2L);
+  if (lsh) {
+    m2L = (m2L << lsh) | (m1L >> (64-lsh));
+  }
+  beL -= lsh;
+
+  // normalize m2U:M1U
+  lsh = __builtin_clzll(m2U);
+  if (lsh) {
+    m2U = (m2U << lsh) | (m1U >> (64-lsh));
+    m1U = (m1U << lsh);
+  }
+  beU -= lsh;
+  m2U |= ((m1U|m0U) != 0); // round up
+
+  *done = true;
+  uint64_t resL = ldexp_u(m2L, beL);
+  if (m2L != m2U || beL != beU)
+    *done = ldexp_u(m2U, beU)==resL;
+
+  return resL;
+}
